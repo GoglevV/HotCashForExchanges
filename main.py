d235276f6d6cfdf4e351302b61a8b534724a0689
Vladimir <vladimir.goglev@gmail.com>
2022-08-04 18:34:02 +0300

    first set to redis

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import pyodbc
 import config
+import redis
+import json
 
 
-
+def redisConnect():
+    r = redis.Redis(host=config.redis_server, port=6379, password=config.redis_password, username=config.redis_user)
+    return r
+    # print(r.connection)
+    # r.set("England", "Manchester")
+    # r.mset({"Croatia": "Zagreb", "Bahamas": "Nassau"})
+    # r.get("Bahamas")
 
 if __name__ == '__main__':
     cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+config.server+';DATABASE='+config.database+';UID='+config.username+';PWD='+ config.password)
     cursor = cnxn.cursor()
     cursor.execute('SELECT TOP 5 [code], [Остаток] as Count, [Цена] as Price FROM sales.w_остаток_как_на_сайте')
     row = cursor.fetchone()
+    rc = redisConnect()
+    uts = {}
+
+
     while row:
-        print(row[0], row[1], row[2])
+        uts.update({str(row[0]): {"Count": float(row[1]), "Price": float(row[2])}})
         row = cursor.fetchone()
+
+    print(uts)
+    with rc.pipeline() as pipe:
+        for ut_id, properties in uts.items():
+            rc.set(ut_id, json.dumps(properties))
+
+    pipe.execute()
+    rc.get(ut_id)
+
 
 
